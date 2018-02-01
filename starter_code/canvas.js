5918fa12567d2c92cd03a6a870f07468d942f85b
MARTIN GENET <martingenet@MHG.local>
2018-02-01 18:02:29 -0500

    hangman did ok

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -4,14 +4,37 @@ function HangmanCanvas(secretWord) {
 }
 
 HangmanCanvas.prototype.createBoard = function () {
+  // First, we need to draw the board. We should clear the canvas, so every time we start the game 
+  // we have a clean one. We can also set up here the width we want to set to our lines.
+  this.ctx.clearRect(0, 0, 1200, 800);
 
 };
 
 HangmanCanvas.prototype.drawLines = function () {
-
+// We know the secret word so we should put one line for each of the letters the user has to guess.
+  let x = 100
+  // this.ctx.moveTo(0, 5);
+    this.ctx.beginPath();
+  for (var i = 0; i<=this.secretWord.length; i++) {
+    this.ctx.moveTo(x, 100);
+    x+=30;
+    this.ctx.lineTo(x, 100);
+    this.ctx.stroke();
+    x+=10;
+  }
 };
 
 HangmanCanvas.prototype.writeCorrectLetter = function (index) {
+// Write the Guessed Letters and the Wrong Ones Every time a user clicks the keyboard, 
+// after checking if the letter was not already clicked, we should write it on our board. If the secret word 
+// includes the letter we should write it in the position it corresponds, and if is not included in the secret word, we will write 
+// it on the top right so the user knows which ones he already clicked.
+
+  this.font = '14px serif';
+  this.fillText(this.guessedLetter+, 800, 200);
+  
+}draw();
+
 
 };
 
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,42 +1,82 @@
 var hangman;
 
-// function Hangman() {
+function Hangman() {
+  this.words=["ironhack", "good", "grandstand"];
+  this.secretWord='';
+  this.letters = [];
+  this.guessedLetter='';
+  this.errorsLeft=10;
 
-// }
+}
 
-// Hangman.prototype.getWord = function () {
+Hangman.prototype.getWord = function () {
+  // getWord(). Returns a random word from our array words.
+  let number = Math.floor(Math.random(words.length));
+  this.secretWord = this.words[number];
+  return this.secretWord;
 
-// };
+};
 
-// Hangman.prototype.checkIfLetter = function (keyCode) {
+Hangman.prototype.checkIfLetter = function (keyCode) {
+  // checkIfLetter. This function should check if the key the user has typed is a letter.
+  if (keyCode < 91 && keyCode > 64){
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// };
+Hangman.prototype.checkClickedLetters = function (key) {
+  // checkClickedLetters. Checks if the pressed letter has already been pressed and returns true if it was not or false in the opposite case.
+  if (this.letters.includes(key)) {
+    return false;
+  } else{
+    return true;
+  }
+};
 
-// Hangman.prototype.checkClickedLetters = function (key) {
+Hangman.prototype.addCorrectLetter = function (i) {
+  // addCorrectLetter. Adds to the guessedLetter variable the letter that was pressed. Also, it should check if the user wins.
+  if(this.secretWord.includes())
+  this.guessedLetter += fromCharCode(i);
+  this.checkWinner();
+  
+};
 
-// };
+Hangman.prototype.addWrongLetter = function (letter) {
+    this.errorsLeft -= 1;
+};
 
-// Hangman.prototype.addCorrectLetter = function (i) {
+Hangman.prototype.checkGameOver = function () {
+  // checkGameOver. Returns a bolean value, true if the users lose, and false in any other case.
+if (this.errorsLeft === 0){
+  return true;
+}else{
+  return false;
+}
+};
 
-// };
-
-// Hangman.prototype.addWrongLetter = function (letter) {
-
-// };
-
-// Hangman.prototype.checkGameOver = function () {
-
-// };
-
-// Hangman.prototype.checkWinner = function () {
-
-// };
+Hangman.prototype.checkWinner = function () {
+  if (this.secretWord.length === this.guessedLetter.length){
+    return true;
+  } else{
+    return false;
+  }
+};
 
 document.getElementById('start-game-button').onclick = function () {
   hangman = new Hangman();
+  canvas.drawHangman();
+  canvas.drawLines;
 };
 
 
 document.onkeydown = function (e) {
-
+  hangman.checkIfLetter(e.keyCode);
+  hangman.checkClickedLetters(e.key);
+  if hangman.secretWord.includes(e){
+    hangman.addCorrectLetter(e.keyCode)
+  }else{
+    hangman.addWrongLetter (e.keyCode);
+  }
 };
